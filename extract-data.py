2dc625aad3f54a32f70cb7d43210fd2aa1951b5b
Dhika Anbiya <danbiya@156-17-130-132.ii.pwr.edu.pl>
2025-02-05 11:37:37 +0100

    create script for extract scrape data to csv

---
+++ extract-data.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+import csv
+import argparse
+
+def list_files_in_folder(folder_path):
+    path = Path(folder_path)
+    return [str(file) for file in path.glob('**/*') if file.is_file()]
+
+# Function to extract all unique zip urls
+def extract(data, filename, output):
+    # Define the CSV file path
+    with open(output, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['cwe', 'download_url'])
+        for entry in data.get('testCases', []): 
+            downloadurl = entry.get('download')
+            for d in entry.get('sarif').get('runs', []):
+                for cwe in d.get('results', []):
+                    if not cwe.get('ruleId') or cwe.get('ruleId').lower() == 'none':
+                        cwe_code = d.get('properties').get('description').split(':')[0]
+                    else:
+                        cwe_code = cwe.get('ruleId')
+                    writer.writerow([cwe_code, downloadurl])
+    print(f"{filename}, exported")
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Extract data from JSON files and export to CSV.')
+    parser.add_argument('--json_path', type=str, required=True, help='Path to the folder containing JSON files')
+    parser.add_argument('--output', type=str, required=True, help='Path to the output CSV file')
+    args = parser.parse_args()
+
+    # Load the JSON data
+    for filename in list_files_in_folder(args.json_path):               
+        with open(filename, 'r') as f:
+            data = json.load(f)      
+            # Extract data
+            extract(data, filename=filename, output=args.output)
+
+if __name__ == "__main__":
+    main()
